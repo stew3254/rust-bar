a7052a88cac4669f4d291c33b57aaf04b8034fe9
stew3254 <rtstewy@gmail.com>
2021-01-14 16:39:49 -0500

    Add configuration to display in yml, add a few base functions to do builtins in main, and reformat code

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,165 @@
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Header {
   pub version: u32,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub stop_signal: Option<u16>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub cont_signal: Option<u16>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub click_events: Option<bool>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Field {
   pub full_text: Option<String>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub short_text: Option<String>,
-  // Custom, not a part of i3 protocol
-  #[serde(skip_serializing)]
-  pub command: Option<Vec<String>>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub color: Option<String>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub background: Option<String>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub border: Option<String>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub border_top: Option<u32>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub border_right: Option<u32>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub border_bottom: Option<u32>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub border_left: Option<u32>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub min_width: Option<u32>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub align: Option<String>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub name: Option<String>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub instance: Option<String>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub urgent: Option<bool>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub separator: Option<bool>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub separator_block_width: Option<u32>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub markup: Option<String>,
+
+  // Custom fields that aren't part of the i3 protocol
+  pub builtin: Option<bool>,
+  pub command: Option<Vec<String>>,
+  pub full_text_format: Option<String>,
+  pub short_text_format: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct I3Config {
+  pub header: Header,
+  pub blocks: Vec<HashMap<String, Field>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Component {
+  pub label: Option<String>,
+  pub temp: Option<char>,
+  pub critical_temp: Option<char>,
+  pub max_temp: Option<char>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CPU {
+  pub name: Option<String>,
+  pub frequency: Option<String>,
+  pub usage: Option<String>,
+  pub vendor_id: Option<u64>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Disk {
+  pub name: Option<String>,
+  pub avail: Option<char>,
+  pub used: Option<char>,
+  pub free: Option<char>,
+  pub total: Option<char>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Memory {
+  pub avail: Option<char>,
+  pub used: Option<char>,
+  pub free: Option<char>,
+  pub total: Option<char>,
+  pub avail_swap: Option<char>,
+  pub used_swap: Option<char>,
+  pub free_swap: Option<char>,
+  pub total_swap: Option<char>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NetIface {
+  pub name: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Process {
+  pub name: Option<String>,
+  pub cmd: Option<Vec<String>>,
+  pub pid: Option<u64>,
+  pub memory_used: Option<char>,
+  pub virtual_used: Option<char>,
+  pub start_time: Option<char>,
+  pub cpu_usage: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+  pub name: Option<String>,
+  pub groups: Option<Vec<String>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct System {
+  pub uptime: Option<char>,
+  pub boot_time: Option<char>,
+  pub load_avg: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Module {
+  pub components: Option<Vec<Component>>,
+  pub cpus: Option<Vec<CPU>>,
+  pub disks: Option<Vec<Disk>>,
+  pub memory: Option<Memory>,
+  pub netifaces: Option<Vec<NetIface>>,
+  pub processes: Option<Vec<Process>>,
+  pub system: Option<System>,
+  pub users: Option<Vec<User>>,
+}
+
+impl Module {
+  pub fn new() -> Module {
+    Module {
+      components: None,
+      cpus: None,
+      disks: None,
+      memory: None,
+      netifaces: None,
+      processes: None,
+      system: None,
+      users: None
+    }
+  }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ModuleType {
+  #[serde(alias = "components")]
+  Components(Vec<Component>),
+  #[serde(alias = "cpus")]
+  CPUs(Vec<CPU>),
+  #[serde(alias = "disks")]
+  Disks(Vec<Disk>),
+  #[serde(alias = "memory")]
+  Memory(Memory),
+  #[serde(alias = "net")]
+  Net(Vec<NetIface>),
+  #[serde(alias = "processes")]
+  Processes(Vec<Process>),
+  #[serde(alias = "system")]
+  System(System),
+  #[serde(alias = "users")]
+  Users(Vec<User>),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-  pub header: Header,
-  pub blocks: Vec<HashMap<String, Field>>,
+  pub builtin: Vec<ModuleType>,
+  pub i3: I3Config,
 }
--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,119 @@ use std::thread::sleep;
 use std::time::{Duration, Instant};
 
 use i3status_bar::*;
-
-// use chrono;
+use chrono::prelude::*;
+use sysinfo::{SystemExt, DiskExt, ProcessorExt};
 // use handlebars;
 
+fn read_config(file_name: &str) -> Result<(Module, Vec<Field>), anyhow::Error> {
+  // Panic if config file can't be opened or read correctly
+  let f = File::open(file_name).unwrap();
+  let conf: Config = serde_yaml::from_reader(f)?;
+
+  // Print out header for the bar
+  // Write '\n[[]' to make things easier to start with
+  println!("{}\n[\n[]", serde_json::to_string(&conf.i3.header)?);
+
+  // Convert the config into the correct i3 format that we can modify
+  let blocks: Vec<Field> = conf.i3.blocks.into_iter().flat_map(|m|
+    m.into_iter().map(|(k, v)|
+      Field {
+        name: Some(k.to_ascii_lowercase()),
+        ..v
+      }
+    )
+  ).collect();
+
+  let mut module = Module::new();
+
+  for module_type in conf.builtin {
+    match module_type {
+      ModuleType::Components(m) => module.components = Some(m),
+      ModuleType::CPUs(m) => module.cpus = Some(m),
+      ModuleType::Disks(m) => module.disks = Some(m),
+      ModuleType::Memory(m) => module.memory = Some(m),
+      ModuleType::Net(m) => module.netifaces = Some(m),
+      ModuleType::Processes(m) => module.processes = Some(m),
+      ModuleType::System(m) => module.system = Some(m),
+      ModuleType::Users(m) => module.users = Some(m),
+    }
+  }
+
+  // Return the modules config and the blocks
+  Ok((module, blocks))
+}
+
+fn gen_closures() -> Vec<Box<dyn FnMut(&mut Field)>> {
+  let mut closures: Vec<Box<dyn FnMut(&mut Field)>> = Vec::new();
+  closures.push(Box::new(|c| {
+    let mut sys = sysinfo::System::new();
+    do_cpu(&mut sys, c);
+  }));
+  // closures.push(|c| {
+  //   let mut sys = System::new();
+  //   do_cpu(&mut sys, c);
+  // });
+  closures
+}
+
+fn do_time(block: &mut Field) {
+  let t = Local::now();
+  // If no normal format found, just do 24 hour time
+  block.full_text =  match &block.full_text_format {
+    Some(f) => Some(t.format(f).to_string()),
+    None => Some(t.format("%H:%M:%S").to_string()),
+  };
+
+  // If no short format found, ignore it
+  block.short_text =  match &block.short_text_format {
+    Some(f) => Some(t.format(f).to_string()),
+    None => None,
+  };
+}
+
+fn do_cpu(sys: &mut sysinfo::System, block: &mut Field) {
+  sys.refresh_cpu();
+  println!("Here");
+}
+
 fn main() -> Result<(), anyhow::Error> {
+  // Get program args
   let args: Vec<_> = env::args().collect();
   let file_name = if args.len() < 2 {
     "i3status.conf"
   } else {
     &args[1]
   };
-  // Panic if config file can't be opened
-  let f = File::open(file_name).unwrap();
 
-  let conf: Config = serde_yaml::from_reader(f)?;
-  // Print out header for the bar 
-  // Write '\n[[]' to make things easier to start with
-  println!("{}\n[\n[]", serde_json::to_string(&conf.header)?);
-  // Convert the config into the correct i3 format that we can modify
-  let mut blocks : Vec<Field> = conf.blocks.into_iter().flat_map(|m|
-    m.into_iter().map(|(k, v)| 
-        Field { 
-          name: Some(k), 
-          ..v
-        }
-    )
-  ).collect();
+  // Get block configuration
+  let (module, mut blocks) = read_config(file_name)?;
   
   // Loop forever to print out the next bar
   loop {
     let start = Instant::now();
     for block in &mut blocks {
-      // Check for commands right now
+      if let Some(name) = &block.name {
+        // Handle all built-in block features
+        if let Some(_) = &block.builtin {
+          match name.as_str() {
+            "time" => {
+              do_time(block);
+            },
+            "cpu" => {
+              // sys.refresh_disks();
+              // for x in sys.get_disks() {
+              // }
+            }
+            _ => {}
+          }
+        }
+      }
+
+      // Check if it's a command
       if let Some(c) = &block.command {
         if c.len() > 0 {
           let mut cmd = Command::new(&c[0]);
-          block.full_text = Some(String::from_utf8(
-            cmd.args(&c[1..]).output()?.stdout
-          )?)
+          block.full_text = Some(String::from_utf8(cmd.args(&c[1..]).output()?.stdout)?)
         }
       }
     }
